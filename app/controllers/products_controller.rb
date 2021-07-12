e28ebbb20a6f7dedb562a13b68119643f99ab296
Rafael Mello <rafael.ferreira.sobral@gmail.com>
2021-07-12 11:33:49 +1000

    Cors

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,45 @@
 class ProductsController < ApplicationController
+    before_action :set_product, except: [:index, :create]
+  
+    rescue_from Exception do |e|
+      render json: { error: e }, status: :not_found
+    end
+  
+    def index
+      render json: Product.all
+    end
+  
+    def show
+      render json: @product
+    end
+  
+    def create
+      render json: Product.create(product_params), status: :created
+    end
+  
+    def update
+      if @product.update(product_params)
+          render json: @product
+      else
+          render json: { error: "Failed to update entry" }, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      if @product.destroy
+          render status: :no_content
+      else
+          render json: { error: "Failed to destroy " }, status: :unprocessable_entity
+      end
+    end
+  
+    private
+  
+    def set_product
+      @product = Product.find(params[:id])
+    end
+  
+    def product_params
+      params.permit(:category_id, :content)
+    end
 end
